d27f501a3c933af87068ff2d17d546ccd381e121
erasmodominguezdc <erasmo_dominguez@discovery.com>
2023-07-02 14:18:09 +0100

    log file

--- vpc_module/variables.tf
+++ vpc_module/variables.tf
@@ -19,9 +19,9 @@ variable "vpc" {
     public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
     enable_ipv6          = true
     enable_nat_gateway   = true
-    enable_vpn_gateway   = false
-    enable_dns_hostnames = true
-    enable_dns_support   = true
+    enable_vpn_gateway   = true
+    enable_dns_hostnames = false
+    enable_dns_support   = false
   }
 }
 
