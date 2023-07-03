ebb7060f991fbaaf2ada7a2f85cf7f16bfd29ef8
erasmodominguezdc <erasmo_dominguez@discovery.com>
2023-07-03 15:57:09 +0100

    adding ecs

--- main.tf
+++ main.tf
@@ -1,19 +1,9 @@
+/***
+module "backend"{
+  source = "./modules/backend"
+}
+**/
 
 module "vpc" {
-  source = "./vpc_module"
-  /**
-  name = var.vpc.name
-  cidr = var.vpc.cidr_block
-
-  azs             = var.vpc.azs
-  private_subnets = var.vpc.private_subnets
-  public_subnets  = var.vpc.public_subnets
-
-  enable_ipv6          = var.vpc.enable_ipv6
-  enable_nat_gateway   = var.vpc.enable_nat_gateway
-  enable_vpn_gateway   = var.vpc.enable_vpn_gateway
-  enable_dns_hostnames = var.vpc.enable_dns_hostnames
-  enable_dns_support   = var.vpc.enable_dns_support
-  tags = var.tags
-  **/
+  source = "./modules/vpc"
 }
---
+++ modules/backend/variables.tf
@@ -0,0 +1,8 @@
+variable "backend" {
+  type = object({
+    bucket_name     = string
+    key             = string
+    region          = string
+    dynamodb_table  = string
+  })
+}
