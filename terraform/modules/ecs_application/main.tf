f4f7ec0c63efa23edf820b8cc2befde7fa8cffa2
erasmodominguezdc <erasmo_dominguez@discovery.com>
2023-07-10 18:30:14 +0100

    dynamodb rle

--- terraform/modules/ecs_application/main.tf
+++ terraform/modules/ecs_application/main.tf
@@ -9,7 +9,65 @@ module "ecs_task_execution_role" {
   iam_role_name = var.ecs_task_execution_role.iam_role_name
   iam_policy_arn = var.ecs_task_execution_role.iam_policy_arn
 }
+## --------------------------------------------------------------------------- ##
+## FIXME. THis Role should be created following the same approach as the rest of the roles ##
+variable "name" {
+  default = "golang-api"
+}
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${var.name}-ecsTaskRole"
 
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "dynamodb" {
+  name        = "${var.name}-task-policy-dynamodb"
+  description = "Policy that allows access to DynamoDB"
+
+  policy = <<EOF
+{
+   "Version": "2012-10-17",
+   "Statement": [
+       {
+           "Effect": "Allow",
+           "Action": [
+               "dynamodb:CreateTable",
+               "dynamodb:UpdateTimeToLive",
+               "dynamodb:PutItem",
+               "dynamodb:DescribeTable",
+               "dynamodb:ListTables",
+               "dynamodb:DeleteItem",
+               "dynamodb:GetItem",
+               "dynamodb:Scan",
+               "dynamodb:Query",
+               "dynamodb:UpdateItem",
+               "dynamodb:UpdateTable"
+           ],
+           "Resource": "*"
+       }
+   ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.dynamodb.arn
+}
 ## --------------------------------------------------------------------------- ##
 
 resource "aws_ecs_task_definition" "ecs_task" {
