00255a31ece0053aab259a7cdb8248b6a0693569
CristianQS <cristian_31teror@hotmail.com>
2024-02-11 11:04:39 +0000

    Fix terraform plan, adding environment var

--- terraform/modules/backend/variables.tf
+++ terraform/modules/backend/variables.tf
@@ -1,7 +1,6 @@
 variable "backend" {
   type = object({
     bucket_name          = string
-    key                  = string
     region               = string
     dynamodb_table       = string
     bucket_sse_algorithm = string
