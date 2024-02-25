d150b0e791e94024eb03f6d6c5722cdbcac18066
Erasmo <erasmolpa@gmail.com>
2024-02-25 08:48:53 +0000

    RED: Test failing because credentials

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,10 @@
-#go mod init ecs-terraform-challenge
-#go mod tidy
+go mod init ecs-terraform-challenge
+
+go mod tidy
 
 terraform init ./terraform
+
 go test backed_test.go
+
+go test ecr_test.go
+
