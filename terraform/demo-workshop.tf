b25ff51296d312ad90a5e475d633ebcb9bab5c87
Erasmo <erasmolpa@gmail.com>
2024-02-25 18:00:26 +0000

    error in lifecycle

--- terraform/demo-workshop.tf
+++ terraform/demo-workshop.tf
@@ -52,20 +52,18 @@ module "alb" {
 module "aws_ecr_repository" {
   source          = "../terraform/modules/ecr_registry"
   repository_name = "serverless-go-app"
-  lifecycle_policy_rules = jsonencode({
-    rules = [{
-      rulePriority = 1
-      description  = "keep last 10 images"
-      action = {
-        type = "expire"
-      }
-      selection = {
-        tagStatus   = "any"
-        countType   = "imageCountMoreThan"
-        countNumber = 10
-      }
-    }]
-  })
+  lifecycle_policy_rules = jsonencode([{
+    rulePriority = 1
+    description  = "keep last 10 images"
+    action = {
+      type = "expire"
+    }
+    selection = {
+      tagStatus   = "any"
+      countType   = "imageCountMoreThan"
+      countNumber = 10
+    }
+  }])
 }
 
 
