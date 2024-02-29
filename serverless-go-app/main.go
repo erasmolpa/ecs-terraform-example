ef43ed30c0c7330841891d7276a70bfcfed4484c
CristianQS <cristian_31teror@hotmail.com>
2024-02-29 00:58:31 +0000

    add mysql connection

--- serverless-go-app/main.go
+++ serverless-go-app/main.go
@@ -1,12 +1,35 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	app.Get("/", func(c fiber.Ctx) error {
+		db, err := sql.Open("mysql",
+			fmt.Sprintf("%s:%s@tcp%s:%s)/%s",
+				os.Getenv("username"),
+				os.Getenv("password"),
+				os.Getenv("address"),
+				os.Getenv("port"),
+				os.Getenv("dbname")))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+		pingErr := db.Ping()
+		if pingErr != nil {
+			log.Fatal(pingErr)
+		}
+		fmt.Println("Connected!")
+		return c.SendString("Hello, World 👋!")
 	})
 
 	app.Listen(":80")
