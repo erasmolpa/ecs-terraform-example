5e5dd461a425f9e654df34f6db004ec0217e3140
Erasmo <erasmolpa@gmail.com>
2024-03-03 08:59:38 +0000

    actions

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,7 +1,3 @@
-# 1 Create remote state
-# 2 Create container registry
-# 3 Build and publish app
-# ---------
 # 4 Create task application
 # 4.1 Create Need VPC
 # 4.2 Create Load Balancer
@@ -13,19 +9,6 @@
 # Separate Cluster Creation https://blog.knoldus.com/how-to-use-data-source-in-terraform/
 # Golang connect to RDS https://docs.aws.amazon.com/code-library/latest/ug/go_2_rds_code_examples.html
 # Localstack https://docs.localstack.cloud/user-guide/aws/rds/  
-
-# En el archivo main.tf
-module "backend" {
-  source  = "../terraform/modules/backend"
-  backend = var.backend
-}
-
-module "aws_ecr_repository" {
-  source                 = "../terraform/modules/ecr_registry"
-  repository_name        = var.aws_ecr_repository
-  lifecycle_policy_rules = var.aws_ecr_repository_lifecycle_policy_rules
-}
-
 
 module "vpc" {
   source = "../terraform/modules/vpc"
