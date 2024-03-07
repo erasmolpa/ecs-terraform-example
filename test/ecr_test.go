55f058ceb1fe937ff1fa64a0cb07c1e3611eea58
CristianQS <cristian_31teror@hotmail.com>
2024-03-07 00:59:05 +0000

    green: test ecr module

--- test/ecr_test.go
+++ test/ecr_test.go
@@ -2,29 +2,29 @@ package test
 
 import (
 	"testing"
+
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/require"
 )
-func TestEcrIsCreatedWithDefaultValues(t *testing.T){
+
+func TestEcrIsCreatedWithDefaultValues(t *testing.T) {
 	t.Parallel()
+	godotenv.Load()
 	repositoryName := "test_ecr_repo"
-    terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../terraform/modules/ecr_registry",
 		Vars: map[string]interface{}{
 			"repository_name": repositoryName,
 		},
 	})
 
-	defer terraform.Destroy(t,terraformOptions)
+	defer terraform.Destroy(t, terraformOptions)
 
-	terraform.InitAndApply(t,terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
 	actualRepositoryName := terraform.Output(t, terraformOptions, "repository_name")
 	require.Equal(t, repositoryName, actualRepositoryName)
 
 	repositoryURL := terraform.Output(t, terraformOptions, "repository_url")
 	require.NotEmpty(t, repositoryURL)
-
-
-	repositoryEncryptionEnabled := terraform.Output(t, terraformOptions, "repository_encryption_enabled")
-	require.Equal(t, "true", repositoryEncryptionEnabled)
 }
